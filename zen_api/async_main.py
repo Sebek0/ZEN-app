d3578ce58c0e0f8e34158d594cba19466abdd8f9
Sebek0 <anaruk6@gmail.com>
2023-04-26 00:41:46 +0200

    Update main async function

--- zen_api/async_main.py
+++ zen_api/async_main.py
@@ -4,72 +4,185 @@ import urllib
 import json
 import time
 
-import aiohttp  # pip install aiohttp aiodns
+import aiohttp  
 
 from dotenv import load_dotenv
+from bungie_api_wrapper import BAPI
 
 load_dotenv()
 
-bungie_key = os.getenv('BUNGIE_API_KEY')
+API_KEY = os.getenv('BUNGIE_API_KEY')
 
-async def get(
-    session: aiohttp.ClientSession,
-    bungie_id: int,
-    membershipType: int,
-    **kwargs
-) -> dict:
-    ################### GET PROFILE FROM BUNGIE ##################################
-    url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{bungie_id}/?components=100"
-    headers = {'X-API-Key': str(bungie_key)}
-    encoded_url = urllib.parse.quote(url, safe=':/?&=,.')
+
+async def get_characters(destiny_membership_id: int, platform: int):
+    """Return characters information in hash values.
     
-    print(f"Requesting {url}")
-    response = await session.get(url=encoded_url, headers=headers, **kwargs)
-    data = await response.json()
-    print(f"Received data for {url}")
+    Function will return all information about User characters in hash values.
+    Returned dictionary is ready to be decoded in Destiny manifest.
     
-    ##################  GET CHARACTER FROM BUNGIE #############################
-    characterId = data['Response']['profile']['data']['characterIds']
-    characters = {}
-    characters_coro = []
-    
-    for char in characterId:
-        character_url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{bungie_id}/Character/{char}/?components=200"
-        encoded_url = urllib.parse.quote(character_url, safe=':/?&=,.')
-        character_response = await session.get(url=encoded_url, headers=headers, **kwargs)
-        character = await character_response.json()
-        characters_coro.append(character)
-        print(character["Response"]["character"]["data"]["characterId"])
+    Args:
+        destiny_membership_id (int): Bungie destiny_membership_id.
+        platform (int): Destiny2 membershipType.
         
-        characters[char] = {
-            'characterId': character['Response']['character']['data']['characterId'],
-            'dateLastPlayed': character['Response']['character']['data']['dateLastPlayed']
+    Returns:
+        characters_informations (dict): Informations about characters in hash
+        values.
+    """
+
+    destiny = BAPI(API_KEY)
+    characters_informations = {}
+    
+    # get user profile from API request
+    profile = await destiny.api.get_destiny_profile(destiny_membership_id,
+                                                    platform, [100, 200, 205])
+    
+    characters_ids = profile['Response']['profile']['data']['characterIds']
+    
+    async def character(profile, character_id):
+        """Add character information in hash values to characters dictionary.
+        
+        Function will add all informations about single character in hash values,
+        to characters dictionary. Using this function we can request character
+        endpoint in the same time.
+        
+        Args:
+            profile (dict): Informations about Bungie user profile.
+            character_id (str): Destiny2 user Character ID.
+        """
+        
+        char_data = profile['Response']['characters']['data'][character_id]
+        items_data = profile['Response']['characterEquipment']['data'] \
+            [character_id]['items']
+        
+        items = {}
+        no_weapon_buckets = ['284967655', '1107761855', '1506418338', '2025709351',
+                            '3683254069', '4023194814', '4292445962', '4274335291',
+                            '3284755031']
+        
+        for i in range(len(items_data)):
+            item = items_data
+            try:
+                if not str(item[i]['bucketHash']) in no_weapon_buckets:
+                    item_raw_data = {}
+                    item_resp = await destiny.api.get_item(destiny_membership_id,
+                                                           item[i]['itemInstanceId'],
+                                                           platform, [302, 304, 307])
+                    
+                    i_hash = item_resp['Response']['item']['data']['itemHash']
+                    b_hash = item_resp['Response']['item']['data']['bucketHash']
+                    
+                    common_data = {
+                        'itemHash': i_hash,
+                        'bucketHash': b_hash
+                    }
+                    
+                    item_stats = item_resp['Response']['stats']['data']['stats']
+                    item_perks = item_resp['Response']['perks']['data']['perks']
+                    
+                    item_raw_data['common_data'] = common_data
+                    item_raw_data['stats'] = item_stats
+                    item_raw_data['perks'] = item_perks
+                    
+                    items[b_hash] = item_raw_data
+            except KeyError as k_error:
+                pass
+                # logger.error(f'{k_error} Bucket: {item[i]["bucketHash"]} \
+                #       Item: {item[i]["itemHash"]} Class: {char_data["classHash"]} \
+                #           ItemInstanceId: {item[i]["itemInstanceId"]}')
+                
+        characters_informations[char_data['classHash']] = {
+            'dateLastPlayed': char_data['dateLastPlayed'],
+            'emblemBackgroundPath': char_data['emblemBackgroundPath'],
+            'emblemHash': char_data['emblemHash'],
+            'emblemPath': char_data['emblemPath'],
+            'light': char_data['light'],
+            'minutesPlayedTotal': char_data['minutesPlayedTotal'],
+            'raceHash': char_data['raceHash'],
+            'stats': char_data['stats'],
+            'items': items
         }
-        
-    print(characters)
-    return characters_coro
+    
+    
+    await asyncio.gather(
+        *[character(profile, char_id) for char_id in characters_ids]
+    )
+    
+    await destiny.close()
+    with open('characters.json', 'w') as file:
+        json.dump(characters_informations, indent=2, sort_keys=True, fp=file)
+    print(characters_informations)
+    return characters_informations
+    
 
-
-async def main(bungie_ids, **kwargs):
-    # Asynchronous context manager.  Prefer this rather
-    # than using a different session for each GET request
+async def main(destiny_membership_ids: int):
+    # Asynchronous context manager
     async with aiohttp.ClientSession() as session:
         profiles = []
-        for bungie_id in bungie_ids:
-            profiles.append(get(session=session, bungie_id=bungie_id, membershipType=3,
-                                **kwargs))
-        # asyncio.gather() will wait on the entire task set to be
-        # completed.  If you want to process results greedily as they come in,
-        # loop over asyncio.as_completed()
+        for destiny_membership_id in destiny_membership_ids:
+            profiles.append(get_characters(
+                                destiny_membership_id=destiny_membership_id,
+                                platform=3
+                                ))
         htmls = await asyncio.gather(*profiles, return_exceptions=True)
         return htmls
 
 
 if __name__ == '__main__':
-    colors = [4611686018468563973, 4611686018468563973, 4611686018468563973,
-              4611686018468563973, 4611686018468563973, 4611686018468563973,
-              4611686018468563973, 4611686018468563973, 4611686018468563973]  # ...
-    # Either take colors from stdin or make some default here
+    destiny_membership_ids = [4611686018468563973, 4611686018468563973, 4611686018468563973,
+                              4611686018468563973, 4611686018468563973, 4611686018468563973]  # ...
     start_time = time.time()
-    asyncio.run(main(colors))  # Python 3.7+
+    asyncio.run(main(destiny_membership_ids))  
     print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+
+
+# DELETE IF NEW VERSION WILL WORK
+# async def get(
+#     session: aiohttp.ClientSession,
+#     bungie_id: int,
+#     membershipType: int,
+#     **kwargs
+# ) -> dict:
+#     ################### GET PROFILE FROM BUNGIE ##################################
+#     url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{bungie_id}/?components=100"
+#     headers = {'X-API-Key': str(bungie_key)}
+#     encoded_url = urllib.parse.quote(url, safe=':/?&=,.')
+    
+#     print(f"Requesting {url}")
+#     response = await session.get(url=encoded_url, headers=headers, **kwargs)
+#     data = await response.json()
+#     print(f"Received data for {url}")
+    
+#     ##################  GET CHARACTER FROM BUNGIE #############################
+#     characterId = data['Response']['profile']['data']['characterIds']
+#     characters = {}
+#     characters_coro = []
+    
+#     for char in characterId:
+#         character_url = f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{bungie_id}/Character/{char}/?components=200"
+#         encoded_url = urllib.parse.quote(character_url, safe=':/?&=,.')
+#         character_response = await session.get(url=encoded_url, headers=headers, **kwargs)
+#         character = await character_response.json()
+#         characters_coro.append(character)
+#         print(character["Response"]["character"]["data"]["characterId"])
+        
+#         characters[char] = {
+#             'characterId': character['Response']['character']['data']['characterId'],
+#             'dateLastPlayed': character['Response']['character']['data']['dateLastPlayed']
+#         }
+        
+#     print(characters)
+#     return characters_coro
+
+
+# async def main(bungie_ids, **kwargs):
+#     # Asynchronous context manager
+#     async with aiohttp.ClientSession() as session:
+#         profiles = []
+#         for bungie_id in bungie_ids:
+#             profiles.append(get(session=session, bungie_id=bungie_id, membershipType=3,
+#                                 **kwargs))
+#         htmls = await asyncio.gather(*profiles, return_exceptions=True)
+#         return htmls
