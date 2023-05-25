73076a999f50e3796ab8f0ca5486c1f6e8d0355c
Sebek0 <anaruk6@gmail.com>
2023-05-25 21:37:09 +0200

    Remove declarative_base import and remove Base variable

--- zen_api/database.py
+++ zen_api/database.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Dependency
@@ -18,4 +17,3 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
